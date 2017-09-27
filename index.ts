5074d2c8869d65332364a12f600b1441dc888451
Carlo Jeske <jeske@sc-networks.com>
2017-09-27 15:24:07 +0200

    [api] improved the initialstatehandling and added fluentinterface possiblity

--- index.ts
+++ index.ts
@@ -1,20 +1,23 @@
-export default class RedChain <stateType, actionType>{
-  private currentState: stateType | null;
+export default class RedChain<stateType, actionType>{
+  public state: stateType;
   private reducers: ((state: stateType | null, action: actionType, previousState?: stateType | null) => stateType)[];
-  constructor(reducers: ((state: stateType | null, action: actionType, previousState?: stateType | null) => stateType)[] = []) {
-    this.currentState = null;
-    this.reducers = reducers;
+  constructor(reducer: ((state: stateType | null, action: actionType, previousState?: stateType | null) => stateType)) {
+    this.reducers = [reducer];
   }
 
   public dispatch(action: actionType) {
-    let currentState = this.currentState;
+    let currentState = this.state || null;
     for (let i = 0; i < this.reducers.length; i += 1) {
-      currentState = this.reducers[i](currentState, action, this.currentState);
+      currentState = this.reducers[i](currentState, action, this.state || null);
     }
-    this.currentState = currentState;
+    this.state = currentState;
+
+    return this;
   }
 
   public addReducer(reducer: (state: stateType | null, action: actionType) => stateType) {
     this.reducers.push(reducer);
+
+    return this;
   }
 }
