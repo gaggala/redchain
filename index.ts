3177569d6cf4fecff4fb54bb3c21e12d8356411a
plusgut <github@webentwickler2-0.de>
2017-09-29 08:44:19 +0200

    [onChange] added callback for change listeners

--- index.ts
+++ index.ts
@@ -1,22 +1,37 @@
 export default class RedChain<stateType, actionType>{
   public state: stateType;
   private reducers: ((state: stateType | null, action: actionType, previousState?: stateType | null) => stateType)[];
+  private onChanges: (() => void)[];
   constructor(reducer: ((state: stateType | null, action: actionType, previousState?: stateType | null) => stateType)) {
     this.reducers = [reducer];
+    this.onChanges = [];
   }
 
   public dispatch(action: actionType) {
     let currentState = this.state || null;
-    for (let i = 0; i < this.reducers.length; i += 1) {
-      currentState = this.reducers[i](currentState, action, this.state || null);
+
+    this.reducers.forEach((reducer) => {
+      currentState = reducer(currentState, action, this.state || null);
+    });
+
+    if (this.state !== currentState) {
+      this.state = currentState;
+      this.onChanges.forEach((onChange) => {
+        onChange();
+      });
     }
-    this.state = currentState;
 
     return this;
   }
 
   public addReducer(reducer: (state: stateType | null, action: actionType) => stateType) {
     this.reducers.push(reducer);
+
+    return this;
+  }
+
+  public addOnChange(onChange: () => void) {
+    this.onChanges.push(onChange);
 
     return this;
   }
