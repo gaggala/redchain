c5393f0418a255a40f8411143520b81caa109457
Carlo Jeske <jeske@sc-networks.com>
2017-12-14 16:48:08 +0100

    [functional] made it a functional approach

--- index.ts
+++ index.ts
@@ -2,62 +2,68 @@ type initialAction = {
   type: 'store::init';
 };
 
-type onChangeCallback<actionType> = ((lastAction: actionType) => void);
+export type onChangeCallback<actionType> = ((lastAction: actionType) => void);
 
-interface reducer<stateType, actionType> {
+export interface reducer<stateType, actionType> {
   (state: stateType | null, action: actionType | initialAction): stateType;
 }
 
-export default class Store<stateType, actionType>{
-  public state: stateType;
-  private initialAction: initialAction = { type: 'store::init' };
-  private reducer: reducer<stateType, actionType>;
-  private onChanges: onChangeCallback<actionType>[];
-  constructor(reducer: reducer<stateType, actionType>) {
-    this.reducer = reducer;
-    this.onChanges = [];
-    this.state = this.reducer(null, this.initialAction);
-  }
+export interface store {
+  <stateType, actionType>(reducer: reducer<stateType, actionType>): storeResult<stateType, actionType>;
+}
 
-  /**
-   * this function triggers the reducer
-   * when the returnvalue is unequal to the previous state it will trigger the listeners from addOnChange
-   */
-  public dispatch(action: actionType) {
-    const currentState = this.reducer(this.state, action);
+export interface storeResult<stateType, actionType> {
+  state: stateType;
+  dispatch(action: actionType): storeResult<stateType, actionType>;
+  addOnChange(onChange: onChangeCallback<actionType>): storeResult<stateType, actionType>;
+  removeOnChange(removeOnChange: onChangeCallback<actionType>): boolean;
+}
 
-    if (this.state !== currentState) {
-      this.state = currentState;
-      this.onChanges.forEach((onChange) => {
-        // currently no other listeners will get notified, when the following line will fuck up
-        // try-catch should be avoided, to improve debuggability
-        // setTimeout would break the call-stack
-        // If you have an opinion on this matter, please make a github issue and tell me
-        onChange(action);
-      });
+const store: store = <stateType, actionType>(reducer: reducer<stateType, actionType>): storeResult<stateType, actionType> => {
+  let onChanges: onChangeCallback<actionType>[] = [];
+
+  return {
+    state: reducer(null, { type: 'store::init' }),
+    /**
+     * this function triggers the reducer
+     * when the returnvalue is unequal to the previous state it will trigger the listeners from addOnChange
+     */
+    dispatch(action: actionType): storeResult<stateType, actionType> {
+      const currentState = reducer(this.state, action);
+
+      if (this.state !== currentState) {
+        this.state = currentState;
+        onChanges.forEach((onChange) => {
+          // currently no other listeners will get notified, when the following line will fuck up
+          // try-catch should be avoided, to improve debuggability
+          // setTimeout would break the call-stack
+          // If you have an opinion on this matter, please make a github issue and tell me
+          onChange(action);
+        });
+      }
+
+      return this;
+    },
+    /**
+     * takes listeners, when the reducer returnvalue is triggered they
+     */
+    addOnChange(onChange: onChangeCallback<actionType>): storeResult<stateType, actionType> {
+      onChanges.push(onChange);
+
+      return this;
+    },
+
+    /**
+     * takes listeners, when the reducer returnvalue is triggered they
+     * and returns true, when something changed
+     */
+    removeOnChange(removeOnChange: onChangeCallback<actionType>) {
+      const previousLength = onChanges.length;
+      onChanges = onChanges.filter((currentOnChange: onChangeCallback<actionType>) => currentOnChange !== removeOnChange);
+
+      return previousLength !== onChanges.length;
     }
-
-    return this;
-  }
-
-  /**
-   * takes listeners, when the reducer returnvalue is triggered they
-   */
-  public addOnChange(onChange: onChangeCallback<actionType>) {
-    this.onChanges.push(onChange);
-
-    return this;
-  }
-
-  /**
-   * takes listeners, when the reducer returnvalue is triggered they
-   * and returns true, when something changed
-   */
-  public removeOnChange(removeOnChange: onChangeCallback<actionType>) {
-    const previousLength = this.onChanges.length;
-    this.onChanges = this.onChanges.filter((currentOnChange: onChangeCallback<actionType>) => currentOnChange !== removeOnChange);
-
-    return previousLength !== this.onChanges.length;
   }
 }
 
+export default store;
