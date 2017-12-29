bc63e5e6eb7c168ff6741a1259009b83583a5eb9
plusgut <github@webentwickler2-0.de>
2017-12-29 16:42:04 +0100

    [dispatch] fixed scoping

--- index.ts
+++ index.ts
@@ -50,32 +50,11 @@ export interface storeResult<stateType, actionType> {
 const store: store = <stateType, actionType>(initFunction: init<stateType>, reducer: reducer<stateType, actionType>): storeResult<stateType, actionType> => {
   let onChanges: onChangeCallback<actionType>[] = [];
 
-  return {
+  const result = {
     /**
      * holds the actual value of the current store
      */
     state: initFunction(),
-
-    /**
-     * this function triggers the reducer
-     * when the returnvalue is unequal to the previous state it will trigger the listeners from addOnChange
-     */
-    dispatch(action: actionType): storeResult<stateType, actionType> {
-      const currentState = reducer(this.state, action);
-
-      if (this.state !== currentState) {
-        this.state = currentState;
-        onChanges.forEach((onChange) => {
-          // currently no other listeners will get notified, when the following line will fuck up
-          // try-catch should be avoided, to improve debuggability
-          // setTimeout would break the call-stack
-          // If you have an opinion on this matter, please make a github issue and tell me
-          onChange(action);
-        });
-      }
-
-      return this;
-    },
 
     /**
      * takes listeners, when the reducer returnvalue is triggered they
@@ -96,7 +75,35 @@ const store: store = <stateType, actionType>(initFunction: init<stateType>, redu
 
       return previousLength !== onChanges.length;
     },
+
+    /**
+     * this function triggers the reducer
+     * when the returnvalue is unequal to the previous state it will trigger the listeners from addOnChange
+     */
+    dispatch: null as any,
   };
+
+  /**
+   * dispatch got added after creating the scope for the result object, to bind the function to this scope
+   */
+  result.dispatch = function (this: storeResult<stateType, actionType>, action: actionType) {
+    const currentState = reducer(this.state, action);
+
+    if (this.state !== currentState) {
+      this.state = currentState;
+      onChanges.forEach((onChange) => {
+        // currently no other listeners will get notified, when the following line will fuck up
+        // try-catch should be avoided, to improve debuggability
+        // setTimeout would break the call-stack
+        // If you have an opinion on this matter, please make a github issue and tell me
+        onChange(action);
+      });
+    }
+
+    return this;
+  }.bind(result);
+
+  return result;
 };
 
 export default store;
