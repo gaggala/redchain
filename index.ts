40196d20203410790b8c3fcc86a695dad508cb07
plusgut <github@webentwickler2-0.de>
2018-01-09 18:19:36 +0100

    [api] changed to currying

--- index.ts
+++ index.ts
@@ -1,14 +1,6 @@
 export type onChangeCallback<actionType> = ((lastAction: actionType) => void);
 
 /**
- * gets initially called when a new store gets created
- * 
- */
-export interface init<stateType> {
-  (): stateType;
-}
-
-/**
  * gets called when a dispatch with an action is triggered
  */
 export interface reducer<stateType, actionType> {
@@ -19,12 +11,16 @@ export interface reducer<stateType, actionType> {
  * thats how a complete store is organized
  */
 export interface store {
-  <stateType, actionType>(initFunction: init<stateType>, reducer: reducer<stateType, actionType>): storeResult<stateType, actionType>;
+  <stateType, actionType>(initValue: stateType, reducer: reducer<stateType, actionType>): storeCurry<stateType, actionType>;
 }
 
 /**
  * the return value of a new created store looks like this
  */
+export interface storeCurry<stateType, actionType> {
+  (): storeResult<stateType, actionType>;
+}
+
 export interface storeResult<stateType, actionType> {
   /**
    * this value gets replaced, each time the reducer gets called
@@ -47,63 +43,68 @@ export interface storeResult<stateType, actionType> {
   removeOnChange(removeOnChange: onChangeCallback<actionType>): boolean;
 }
 
-const store: store = <stateType, actionType>(initFunction: init<stateType>, reducer: reducer<stateType, actionType>): storeResult<stateType, actionType> => {
-  let onChanges: onChangeCallback<actionType>[] = [];
+const store: store = <stateType, actionType>(initValue: stateType, reducer: reducer<stateType, actionType>): storeCurry<stateType, actionType> => {
 
-  const result = {
+  const storeCurry: storeCurry<stateType, actionType> = () => {
+    let onChanges: onChangeCallback<actionType>[] = [];
+
+    const result: storeResult<stateType, actionType> = {
+      /**
+       * holds the actual value of the current store
+       */
+      state: initValue,
+
+      /**
+       * takes listeners, when the reducer returnvalue is triggered they
+       */
+      addOnChange(onChange: onChangeCallback<actionType>): storeResult<stateType, actionType> {
+        onChanges.push(onChange);
+
+        return this;
+      },
+
+      /**
+       * takes listeners, when the reducer returnvalue is triggered they
+       * and returns true, when something changed
+       */
+      removeOnChange(removeOnChange: onChangeCallback<actionType>) {
+        const previousLength = onChanges.length;
+        onChanges = onChanges.filter((currentOnChange: onChangeCallback<actionType>) => currentOnChange !== removeOnChange);
+
+        return previousLength !== onChanges.length;
+      },
+
+      /**
+       * this function triggers the reducer
+       * when the returnvalue is unequal to the previous state it will trigger the listeners from addOnChange
+       */
+      dispatch: null as any,
+    };
+
     /**
-     * holds the actual value of the current store
+     * dispatch got added after creating the scope for the result object, to bind the function to this scope
      */
-    state: initFunction(),
-
-    /**
-     * takes listeners, when the reducer returnvalue is triggered they
-     */
-    addOnChange(onChange: onChangeCallback<actionType>): storeResult<stateType, actionType> {
-      onChanges.push(onChange);
-
+    result.dispatch = function (this: storeResult<stateType, actionType>, action: actionType) {
+      const currentState = reducer(this.state, action);
+  
+      if (this.state !== currentState) {
+        this.state = currentState;
+        onChanges.forEach((onChange) => {
+          // currently no other listeners will get notified, when the following line will fuck up
+          // try-catch should be avoided, to improve debuggability
+          // setTimeout would break the call-stack
+          // If you have an opinion on this matter, please make a github issue and tell me
+          onChange(action);
+        });
+      }
+  
       return this;
-    },
+    }.bind(result);
 
-    /**
-     * takes listeners, when the reducer returnvalue is triggered they
-     * and returns true, when something changed
-     */
-    removeOnChange(removeOnChange: onChangeCallback<actionType>) {
-      const previousLength = onChanges.length;
-      onChanges = onChanges.filter((currentOnChange: onChangeCallback<actionType>) => currentOnChange !== removeOnChange);
-
-      return previousLength !== onChanges.length;
-    },
-
-    /**
-     * this function triggers the reducer
-     * when the returnvalue is unequal to the previous state it will trigger the listeners from addOnChange
-     */
-    dispatch: null as any,
+    return result;
   };
 
-  /**
-   * dispatch got added after creating the scope for the result object, to bind the function to this scope
-   */
-  result.dispatch = function (this: storeResult<stateType, actionType>, action: actionType) {
-    const currentState = reducer(this.state, action);
-
-    if (this.state !== currentState) {
-      this.state = currentState;
-      onChanges.forEach((onChange) => {
-        // currently no other listeners will get notified, when the following line will fuck up
-        // try-catch should be avoided, to improve debuggability
-        // setTimeout would break the call-stack
-        // If you have an opinion on this matter, please make a github issue and tell me
-        onChange(action);
-      });
-    }
-
-    return this;
-  }.bind(result);
-
-  return result;
+  return storeCurry;
 };
 
 export default store;
